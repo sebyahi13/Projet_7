73097dfffe3a9705958e8fab0529132f72beb786
seb yahi <sebsebyahi123@gmail.com>
2022-12-27 07:34:33 +0100

    3 commit v1 du projet 7

--- pages/Tableau_de_bord.py
+++ pages/Tableau_de_bord.py
@@ -109,7 +109,7 @@ infos_client = pd.DataFrame({})
 code_gender={0:'FEMME',1:'HOMME'     }
 st.write(   code_gender[ 0  ]     )
 #X_client1['CODE_GENDER'].values
-st.write(   X_client1['CODE_GENDER'].loc[0]    )
+st.write(    X_client1.loc[0, 'CODE_GENDER']      )
 infos_client = infos_client.append({
     'INFORMATION' : 'Sexe du client' ,
     'VALEUR' :        0 # code_gender[  X_client1['CODE_GENDER']           ]  ,

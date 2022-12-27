377cb95d8295ecfd421744201d50642e4f6b2015
seb yahi <sebsebyahi123@gmail.com>
2022-12-27 05:27:47 +0100

    3 commit v1 du projet 7

--- ACCUEIL.py
+++ ACCUEIL.py
@@ -12,9 +12,11 @@ html_txt = """
     <div style="background-color: Cyan; padding:5px; border-radius:8px">
     <h1 style="color: black; text-align:center">HOME CREDIT DEFAULT RISK</h1>
     </div>
-       
-    
-    <div style="background-color: gray; padding:5px; border-radius:8px">
+
+    <div>  
+    </div>
+
+    <div style="background-color: white; padding:5px; border-radius:8px">
         Construire un modèle de scoring qui donnera une prédiction sur la probabilité de faillite d'un client de façon automatique
 
     </div>

88b61aa2b007466f10e9e6cf773f16018b25d99e
seb yahi <sebsebyahi123@gmail.com>
2022-12-27 05:25:29 +0100

    3 commit v1 du projet 7

--- ACCUEIL.py
+++ ACCUEIL.py
@@ -11,12 +11,14 @@ import plotly.graph_objects as go
 html_txt = """
     <div style="background-color: Cyan; padding:5px; border-radius:8px">
     <h1 style="color: black; text-align:center">HOME CREDIT DEFAULT RISK</h1>
+    </div>
+       
+    
+    <div style="background-color: gray; padding:5px; border-radius:8px">
+        Construire un modèle de scoring qui donnera une prédiction sur la probabilité de faillite d'un client de façon automatique
 
-    Construire un modele de scoring qui donnera une prediction sur la probabilité de faillite d'un client de façon automatique
+    </div>
 
-
-
-    </div>    
     """
 st.markdown(html_txt, unsafe_allow_html=True)
 
